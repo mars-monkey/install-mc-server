86e32698ca9e05c78af7fa8193413bac6fa74fe2
starship-boi <91227993+starship-boi@users.noreply.github.com>
2022-05-05 13:46:08 -0700

    Update REMOVE.sh

--- REMOVE.sh
+++ REMOVE.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 # Check for root
+mkdir /tmp/test_perms
 
-# Remove /usr/bin entry
+if [ -d /tmp/test_perms ]
+then
+	rmdir /tmp/test_perms
+else
+	echo "This script was not run as root. You must run it as root to remove the Minecraft server."
+fi
 
-# Delete install-mc-server and mc-server directories
+rm /usr/bin/mc-start
 
-# Uninstall java?
+cd ..
+rm -rf install-mc-server mc-server
 
-# Uninstall git?
+echo "Would you like the script to remove openjdk-17-jre-headless? (y/N)"
+read rm_java
+
+if [[ $rm_java == "y"]]
+then
+	apt-get remove openjdk-17-jre-headless -y 1>/dev/null
+fi
+
+echo "Would you like the script to remove git? (y/N)"
+read rm_git
+
+if [[ $rm_git == "y"]]
+then
+	apt-get remove git -y 1>/dev/null
+fi
